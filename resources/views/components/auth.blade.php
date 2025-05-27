bad8408bf463d1d25fa2fb0c0f25e74fed31f6e1
achdianaja <rizkiachdian@gmail.com>
2025-05-27 22:45:30 +0700

    View:adding page login

---
+++ resources/views/components/auth.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+
+<x-head></x-head>
+
+<body x-data="{ page: 'comingSoon', 'loaded': true, 'darkMode': false, 'stickyMenu': false, 'sidebarToggle': false, 'scrollTop': false }" x-init="darkMode = JSON.parse(localStorage.getItem('darkMode'));
+$watch('darkMode', value => localStorage.setItem('darkMode', JSON.stringify(value)))" :class="{ 'dark bg-gray-900': darkMode === true }">
+    <!-- ===== Preloader Start ===== -->
+    <div x-show="loaded" x-init="window.addEventListener('DOMContentLoaded', () => { setTimeout(() => loaded = false, 500) })"
+        class="fixed left-0 top-0 z-999999 flex h-screen w-screen items-center justify-center bg-white dark:bg-black">
+        <div class="h-16 w-16 animate-spin rounded-full border-4 border-solid border-brand-500 border-t-transparent">
+        </div>
+    </div>
+
+    <!-- ===== Preloader End ===== -->
+
+    <!-- ===== Page Wrapper Start ===== -->
+    <div class="relative p-6 bg-white z-1 dark:bg-gray-900 sm:p-0">
+        <div class="relative flex flex-col justify-center w-full h-screen dark:bg-gray-900 sm:p-0 lg:flex-row">
+            {{ $slot }}
+        </div>
+    </div>
+    <!-- ===== Page Wrapper End ===== -->
+    <script defer src="{{ asset('tailadmin/build/bundle.js') }}"></script>
+    <script defer src="{{ asset('fontawesome/pro.min.js') }}"></script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -12,3 +13,4 @@ Route::get('/', [AdminController::class, 'index'])->name('dashboard');
 Route::get('/cashiers', [AdminController::class, 'cashiers'])->name('cashiers');
 Route::get('/pill', [AdminController::class, 'pill'])->name('pill');
 Route::get('/history', [AdminController::class, 'history'])->name('history');
+Route::get('/login', [AuthController::class, 'login'])->name('login');
