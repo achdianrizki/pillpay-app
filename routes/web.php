831cb7638f72452131750be2dc1efc2384359620
Anggowww <anggamaulanarahman18@gmail.com>
2025-05-27 23:13:05 +0700

    View:adding style page login

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
 
 // Route::get('/', function () {
 //     $data = "hallo anggow";
